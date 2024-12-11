be948637755fadb76ff5efad3ac01c784bdffae8
Ziusz <podengo@tlen.pl>
2024-12-11 05:33:19 +0100

    Add Daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -20,5 +21,24 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography, daisyui],
+    
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    "primary": "#0ea5e9",
+                    "secondary": "#7c3aed",
+                    "accent": "#37cdbe",
+                    "neutral": "#3d4451",
+                    "base-100": "#ffffff",
+                    "info": "#3abff8",
+                    "success": "#36d399",
+                    "warning": "#fbbd23",
+                    "error": "#f87272"
+                }
+            }
+        ],
+        darkTheme: false,
+    }
 };
